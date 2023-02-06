4358ebe37a3a6d4b66dbc33b1440d8c009543084
GitHub Action <action@github.com>
2023-02-06 15:12:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/rhanafy/validator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rhanafy/validator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rhanafy.validator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rhanafy.validator.MyBundle
 
 class MyApplicationService {
 
